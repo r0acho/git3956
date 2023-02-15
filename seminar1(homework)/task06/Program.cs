903007cf1b6dc827e5cc2d5de0738dade4b46be8
Nikita <nikitarelkin@gmail.com>
2023-02-15 20:28:08 +0300

    ДЗ по первому семинару

---
+++ seminar1(homework)/task06/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число");
+int x = Convert.ToInt32(Console.ReadLine());
+isdigit(x);
+
+void isdigit(int num) 
+{
+    if(num%2 == 0)
+    {
+        Console.WriteLine("да");
+    }
+    else 
+    {
+        Console.WriteLine("нет");
+    }
+}
